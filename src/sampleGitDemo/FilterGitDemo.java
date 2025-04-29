85cb40d0c9b34a25b72a7cf710ddd61f84576038
swetha <swethacse2002@gmail.com>
2025-04-29 13:27:29 +0530

    commit msg from terminal gitdemo

---
+++ src/sampleGitDemo/FilterGitDemo.java
@@ -0,0 +1,9 @@
+package sampleGitDemo;
+
+//Example for 'filter' method in java stream under intermediate operations
+
+public class FilterGitDemo {
+    public static void main(String[] args) {
+
+    }
+}
