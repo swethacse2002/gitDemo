c94dc10eb870f692848426db9bea075bb40b56e9
swetha <swethacse2002@gmail.com>
2025-04-29 13:31:06 +0530

    Created git demo branch 1

---
+++ src/sampleGitDemo/demoBranch1.java
@@ -0,0 +1,9 @@
+package sampleGitDemo;
+//demo branch 1
+//Example for 'filter' method in java stream under intermediate operations
+
+public class demoBranch1 {
+    public static void main(String[] args) {
+
+    }
+}
